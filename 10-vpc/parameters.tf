8703d843b7d0686a5af1d3b2d958eaf228e9cfbd
cherry2501 <saicharangudiya@gmail.com>
2025-01-25 16:05:27 +0530

    terraform

--- 10-vpc/parameters.tf
+++ 10-vpc/parameters.tf
@@ -3,3 +3,24 @@ resource "aws_ssm_parameter" "vpc_id" {
   type = "String"
   value = module.vpc.vpc_id
 }
+
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/public_subnet_ids"
+  type = "StringList"
+  value = join(",",module.vpc.public_subnet_ids)
+  
+}
+
+resource "aws_ssm_parameter" "backend_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/backend_subnet_ids"
+  type = "StringList"
+  value = join(",",module.vpc.private_subnet_ids)
+  
+}
+
+resource "aws_ssm_parameter" "database_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/mysql_subnet_ids"
+  type = "StringList"
+  value = join(",",module.vpc.database_subnet_ids)
+  
+}
