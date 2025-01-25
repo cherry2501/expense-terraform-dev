c15edb57dfe9f3d3868157655129c081c4820e0e
cherry2501 <saicharangudiya@gmail.com>
2025-01-25 15:26:43 +0530

    terraform

---
+++ 40-apps/variables.tf
@@ -0,0 +1,38 @@
+variable "project_name" {
+  default = "expense"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+
+    }
+  
+}
+
+variable "mysql_tags" {
+  default = {
+    Component = "mysql"
+  }
+}
+
+variable "backend_tags" {
+  default = {
+    Component = "backend"
+  }
+}
+
+variable "frontend_tags" {
+  default = {
+    Component = "frontend"
+  }
+}
+
+variable "ansible_tags" {
+  default = {
+    Component = "ansible"
+  }
+}
